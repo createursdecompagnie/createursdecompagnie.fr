75b02a7f878676ac7e54dd87939273651b8c9ed0
Frédéric Batardy <frederic.batardy@gmail.com>
2025-10-22 18:35:24 +0200

    Remove external link icon
    (cherry picked from commit a06259547eddee16f0fbf4ea0230cf76c822a6a6)

---
+++ src/theme/Icon/ExternalLink/index.tsx
@@ -0,0 +1,11 @@
+import React, {type ReactNode} from 'react';
+import type {Props} from '@theme/Icon/ExternalLink';
+
+export default function IconExternalLink({
+  width = 13.5,
+  height = 13.5,
+}: Props): ReactNode {
+  return (
+    <></>
+  );
+}
