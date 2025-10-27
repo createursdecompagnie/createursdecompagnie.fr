b3bf61cff4023865f9cb7abb76064971e2ec6292
Frédéric Batardy <frederic.batardy@gmail.com>
2025-10-27 15:36:52 +0100

    Fix link faire un don

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -80,8 +80,8 @@ const config: Config = {
           ],
         },
         {
-          href: 'https://streamlabscharity.com/teams/@createurs-de-compagnie-2025/cdc2025?member=452020463900692480',
-          label: 'Donner 🎃',
+          href: 'https://streamlabscharity.com/teams/@createurs-de-compagnie-2025/cdc2025',
+          label: 'Faire un don 🎃',
           position: 'right',
           className: 'donate-btn margin-right--md',
         },
